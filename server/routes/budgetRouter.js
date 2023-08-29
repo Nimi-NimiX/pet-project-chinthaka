f55be4fe77b9292b39b159749c022d9e451df4a7
Chinthaka Kasun <chnthkksn@gmail.com>
2023-08-29 12:12:12 +0530

    created middleware to extract month from budgetId

--- server/routes/budgetRouter.js
+++ server/routes/budgetRouter.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
+const extractMonth = require('../middleware/extractMonth');
+
 const { getBudget, setBudget } = require('../controllers/budgetController');
 
-router.get('/:id', getBudget);
+router.get('/:id', extractMonth, getBudget);
 
-router.post('/:id', setBudget);
+router.post('/:id', extractMonth, setBudget);
 
 module.exports = router;
