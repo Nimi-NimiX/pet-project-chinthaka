32214f4e55179c35a496d09addb787660a8ac2d4
Chinthaka Kasun <chnthkksn@gmail.com>
2023-09-07 13:42:47 +0530

    Created overview cards and added to overview page

--- client/src/components/Overview/index.js
+++ client/src/components/Overview/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { Box } from '@mui/system';
+import { Store } from '../../utils/store';
+import OverviewCard from './OverviewCard';
 
 const Overiew = () => {
+  // initialize store
+  const store = Store.useContainer();
+
   return (
     <>
-      <Box sx={styles}>Overiew</Box>
+      <Box sx={styles}>
+        <OverviewCard title={'Balance'} amount={store.budget?.balance || 0} />
+        <OverviewCard title={'Income'} amount={store.budget?.income || 0} />
+        <OverviewCard title={'Expense'} amount={store.budget?.expense || 0} />
+        <OverviewCard
+          title={'Estimated Budget'}
+          amount={store.budget.estimatedBudget || 0}
+          editable
+        />
+      </Box>
     </>
   );
 };
